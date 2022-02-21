558a1ec93cc431655fb223f84cb216e8c0357d61
Collene De Silva <collene.desilva@gmail.com>
2022-02-22 07:41:22 +0800

    Clean code from unnecessary comments

--- script.js
+++ script.js
@@ -2,20 +2,17 @@
 let playerScore = 0
 let computerScore = 0
 
-//Generating the turn of the computer
 function computerPlay() {
     const moves = ['Rock', 'Paper', 'Scissors']
 
-    //Randomizing using random() function in the observance of the length of the moves array
+    //randomizing using the fraction of the length
     let computerTurn = moves[Math.floor(Math.random() * moves.length)]
 
     return computerTurn
 }
 
-//Create a playRound function
 function playRound(playerSelection, computerSelection) {
 
-    //Conditionals for who won and who lost including the score
     if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         computerScore += 1
         return `You lost! ${computerSelection} beats ${playerSelection}`
@@ -47,35 +44,29 @@ function playRound(playerSelection, computerSelection) {
        
 }
 
-//Function for restarting scores for every game played
 function resetScore() {
     playerScore = 0;
     computerScore = 0;
 }
 
-//Creating a function to start the game
 function game() {
     
-    //Resetting the scores first
     resetScore()
 
     //5 round play
     for (let i = 0; i < 5; i++) {
 
-        //Get the answer of user
         const answer = prompt('What is your turn?')
 
-        //Capitalize first letter of the answer to match the case of conditions
         const playerSelection = answer.slice(0,1).toUpperCase() + answer.slice(1,).toLowerCase()
         const computerSelection = computerPlay()
 
-        //Invoking playRound
         const result = playRound(playerSelection, computerSelection)
 
         console.log(result)
     }
     
-    //Game over. Displaying the winner.
+    //Game over
     if (playerScore > computerScore) {
         return `You won! Your score: ${playerScore}; Computer score: ${computerScore}`
     } else if (playerScore < computerScore) {
