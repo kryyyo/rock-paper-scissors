d2dd928ca0f0b544cb0e6169f3c8c00a414cf57c
Collene De Silva <collene.desilva@gmail.com>
2022-02-22 05:45:47 +0800

    Add function for computer's turn

---
+++ script.js
@@ -0,0 +1,17 @@
+function computerPlay() {
+    const moves = ['Rock', 'Paper', 'Scissors']
+
+    let computerTurn = moves[Math.floor(Math.random() * moves.length)]
+
+    return computerTurn
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+    }
+}
