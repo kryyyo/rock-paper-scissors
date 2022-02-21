3aabf9cee8e45bfa9b1e2a3274f4da5a5693d1ae
Collene De Silva <collene.desilva@gmail.com>
2022-02-22 06:50:53 +0800

    Added comments for readability

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+//Initialization of Scores (containers)
+let playerScore = 0
+let computerScore = 0
+
+//Generating the turn of the computer
 function computerPlay() {
     const moves = ['Rock', 'Paper', 'Scissors']
 
+    //Randomizing using random() function in the observance of the length of the moves array
     let computerTurn = moves[Math.floor(Math.random() * moves.length)]
 
     return computerTurn
 }
 
-
-
+//Create a playRound function
 function playRound(playerSelection, computerSelection) {
 
+    //Conditionals for who won and who lost including the score
     if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         computerScore += 1
         return `You lost! ${computerSelection} beats ${playerSelection}`
@@ -41,26 +47,35 @@ function playRound(playerSelection, computerSelection) {
        
 }
 
-let playerScore = 0
-let computerScore = 0
+//Function for restarting scores for every game played
+function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+}
 
+//Creating a function to start the game
 function game() {
     
+    //Resetting the scores first
     resetScore()
 
+    //5 round play
     for (let i = 0; i < 5; i++) {
 
+        //Get the answer of user
         const answer = prompt('What is your turn?')
 
+        //Capitalize first letter of the answer to match the case of conditions
         const playerSelection = answer.slice(0,1).toUpperCase() + answer.slice(1,).toLowerCase()
         const computerSelection = computerPlay()
 
+        //Invoking playRound
         const result = playRound(playerSelection, computerSelection)
 
         console.log(result)
     }
     
-    //for winner: 
+    //Game over. Displaying the winner.
     if (playerScore > computerScore) {
         return `You won! Your score: ${playerScore}; Computer score: ${computerScore}`
     } else if (playerScore < computerScore) {
@@ -71,9 +86,4 @@ function game() {
         return 'Something is wrong'
     }
 
-}
-
-function resetScore() {
-    playerScore = 0;
-    computerScore = 0;
 }
