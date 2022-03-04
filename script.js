aa4d21f16a36e6cde481f97b7c49f8df21b9e0b2
Collene De Silva <collene.desilva@gmail.com>
2022-03-05 05:56:55 +0800

    Add Lizard and Spock categories

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let playerScore = 0
 let computerScore = 0
 
 function computerPlay() {
-    const moves = ['Rock', 'Paper', 'Scissors']
+    const moves = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
     //randomizing using the fraction of the length
     let computerTurn = moves[Math.floor(Math.random() * moves.length)]
@@ -15,33 +15,79 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         computerScore += 1
-        return `You lost! ${computerSelection} beats ${playerSelection}`
+        return `You lost! ${computerSelection} covers ${playerSelection}`
+    } else if (playerSelection === 'Rock' && computerSelection === 'Spock') {
+        computerScore += 1
+        return `You lost! ${computerSelection} vaporizes ${playerSelection}`
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         playerScore += 1
-        return `You won! ${playerSelection} beats ${computerSelection}`
+        return `You won! ${playerSelection} crushes ${computerSelection}`
+    }  else if (playerSelection === 'Rock' && computerSelection === 'Lizard') {
+        playerScore += 1
+        return `You won! ${playerSelection} crushes ${computerSelection}`
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         computerScore += 1
-        return `You lost! ${computerSelection} beats ${playerSelection}`
+        return `You lost! ${computerSelection} cuts ${playerSelection}`
+    } else if (playerSelection === 'Paper' && computerSelection === 'Lizard') {
+        computerScore += 1
+        return `You lost! ${computerSelection} eats ${playerSelection}`
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         playerScore += 1
-        return `You won! ${playerSelection} beats ${computerSelection}`
+        return `You won! ${playerSelection} covers ${computerSelection}`
+    } else if (playerSelection === 'Paper' && computerSelection === 'Spock') {
+        playerScore += 1
+        return `You won! ${playerSelection} disproves ${computerSelection}`
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         computerScore += 1
-        return `You lost! ${computerSelection} beats ${playerSelection}`
+        return `You lost! ${computerSelection} crushes ${playerSelection}`
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Spock') {
+        computerScore += 1
+        return `You lost! ${computerSelection} smashes ${playerSelection}`
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         playerScore += 1
-        return `You won! ${playerSelection} beats ${computerSelection}`
+        return `You won! ${playerSelection} cuts ${computerSelection}`
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Lizard') {
+        playerScore += 1
+        return `You won! ${playerSelection} decapitates ${computerSelection}`
+    } else if (playerSelection === 'Lizard' && computerSelection === 'Scissors') {
+        computerScore += 1
+        return `You lost! ${computerSelection} decapitates ${playerSelection}`
+    } else if (playerSelection === 'Lizard' && computerSelection === 'Rock') {
+        computerScore += 1
+        return `You lost! ${computerSelection} crushes ${playerSelection}`
+    } else if (playerSelection === 'Lizard' && computerSelection === 'Paper') {
+        playerScore += 1
+        return `You won! ${playerSelection} eats ${computerSelection}`
+    } else if (playerSelection === 'Lizard' && computerSelection === 'Spock') {
+        playerScore += 1
+        return `You won! ${playerSelection} poisons ${computerSelection}`
+    } else if (playerSelection === 'Spock' && computerSelection === 'Lizard') {
+        computerScore += 1
+        return `You lost! ${computerSelection} poisons ${playerSelection}`
+    } else if (playerSelection === 'Spock' && computerSelection === 'Paper') {
+        computerScore += 1
+        return `You lost! ${computerSelection} disproves ${playerSelection}`
+    } else if (playerSelection === 'Spock' && computerSelection === 'Rock') {
+        playerScore += 1
+        return `You won! ${playerSelection} vaporizes ${computerSelection}`
+    } else if (playerSelection === 'Spock' && computerSelection === 'Scissors') {
+        playerScore += 1
+        return `You won! ${playerSelection} smashes ${computerSelection}`
     } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
         return `It's a tie. You both use ${playerSelection}`
     } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
         return `It's a tie. You both use ${playerSelection}`
     } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
         return `It's a tie. You both use ${playerSelection}`
+    } else if (playerSelection === 'Lizard' && computerSelection === 'Lizard') {
+        return `It's a tie. You both use ${playerSelection}`
+    } else if (playerSelection === 'Spock' && computerSelection === 'Spock') {
+        return `It's a tie. You both use ${playerSelection}`
     } else {
         computerScore += 1
         return `Wrong input! Score goes to Computer`
     }
-       
+
 }
 
 function resetScore() {
